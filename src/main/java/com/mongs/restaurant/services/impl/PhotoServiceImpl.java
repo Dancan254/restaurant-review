e86d0badb0aec88fef88774046f1412cc3e433a7
Dancan254 <dancanian25@gmail.com>
2025-04-14 17:14:16 +0300

    Implement PhotoService interface with upload and retrieval methods

---
+++ src/main/java/com/mongs/restaurant/services/impl/PhotoServiceImpl.java
@@ -0,0 +1,36 @@
+package com.mongs.restaurant.services.impl;
+
+import com.mongs.restaurant.domain.entities.Photo;
+import com.mongs.restaurant.services.PhotoService;
+import com.mongs.restaurant.services.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PhotoServiceImpl implements PhotoService {
+    private final StorageService storageService;
+
+
+    @Override
+    public Photo uploadPhoto(MultipartFile file) {
+        String photoId = UUID.randomUUID().toString();
+        String url = storageService.storeFile(file, photoId);
+
+        return Photo.builder()
+                .url(url)
+                .uploadTime(LocalDateTime.now())
+                .build();
+    }
+
+    @Override
+    public Optional<Resource> getPhotoAsResource(String id) {
+        return storageService.loadFileAsResource(id);
+    }
+}
