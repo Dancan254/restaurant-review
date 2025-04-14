2d28279ef5f690caa34dcb9e3f76b2b4e70d1bf7
Dancan254 <dancanian25@gmail.com>
2025-04-14 08:46:05 +0300

    Add User entity with fields for id, username, givenName, and familyName

---
+++ src/main/java/com/mongs/restaurant/domain/entities/User.java
@@ -0,0 +1,24 @@
+package com.mongs.restaurant.domain.entities;
+
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String username;
+
+    @Field(type = FieldType.Text)
+    private String givenName;
+
+    @Field(type = FieldType.Text)
+    private String familyName;
+}
