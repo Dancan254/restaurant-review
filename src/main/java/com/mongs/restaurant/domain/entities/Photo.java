3803b51460d8f31bc1efa17b57f56e562dfae283
Dancan254 <dancanian25@gmail.com>
2025-04-14 08:45:46 +0300

    Add Photo entity with fields for URL and upload time

---
+++ src/main/java/com/mongs/restaurant/domain/entities/Photo.java
@@ -0,0 +1,23 @@
+package com.mongs.restaurant.domain.entities;
+
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Photo {
+
+    @Field(type = FieldType.Keyword)
+    private String url;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime uploadTime;
+    
+}
