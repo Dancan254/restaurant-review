e02ac6aaf5487a797ea61dd26cf15b0404d585f6
Dancan254 <dancanian25@gmail.com>
2025-04-15 12:12:53 +0300

    Add GlobalExceptionHandler for centralized exception handling

---
+++ src/main/java/com/mongs/restaurant/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.mongs.restaurant.exceptions;
+
+import com.mongs.restaurant.domain.entities.dtos.ErrorDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(StorageException.class)
+    public ResponseEntity<ErrorDto> handleStorageException(StorageException e) {
+        log.error("Storage exception occurred: {}", e.getMessage());
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("Unable to save or retrieve the file.")
+                .build();
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorDto> handleBaseException(BaseException e) {
+        log.error("Base exception occurred: {}", e.getMessage());
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred.")
+                .build();
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //lets catch all other exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception e) {
+        log.error("An unexpected error occurred: {}", e.getMessage());
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred.")
+                .build();
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
