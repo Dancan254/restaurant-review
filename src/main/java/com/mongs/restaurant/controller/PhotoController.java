e788deebd22a96160c1f883f0bb6626a59ee95e0
Dancan254 <dancanian25@gmail.com>
2025-04-15 20:35:33 +0300

    Refactor PhotoController to streamline imports and enhance getPhoto method for improved readability

--- src/main/java/com/mongs/restaurant/controller/PhotoController.java
+++ src/main/java/com/mongs/restaurant/controller/PhotoController.java
@@ -5,11 +5,12 @@ import com.mongs.restaurant.domain.entities.dtos.PhotoDto;
 import com.mongs.restaurant.mappers.PhotoMapper;
 import com.mongs.restaurant.services.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -25,5 +26,20 @@ public class PhotoController {
         Photo photo = photoService.uploadPhoto(file);
 
         return ResponseEntity.ok(photoMapper.toDto(photo));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.getPhotoAsResource(id).map(
+                photo ->
+                        ResponseEntity.ok()
+                                .contentType(
+                                        MediaTypeFactory.getMediaType(photo)
+                                                .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                                )
+                                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                                .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
+
     }
 }
