999354e00c32b14311eb04dd49b97028edaf4ed9
Dancan254 <dancanian25@gmail.com>
2025-04-15 12:12:48 +0300

    Refactor PhotoDto to include Lombok annotations for boilerplate reduction

--- src/main/java/com/mongs/restaurant/domain/entities/dtos/PhotoDto.java
+++ src/main/java/com/mongs/restaurant/domain/entities/dtos/PhotoDto.java
@@ -1,4 +1,16 @@
 package com.mongs.restaurant.domain.entities.dtos;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhotoDto {
+
+    private String url;
+    private LocalDateTime uploadDate;
 }
