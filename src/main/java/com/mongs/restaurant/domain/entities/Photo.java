0cc72c69cbf49528d45cd65f8238ecfd521b346e
Dancan254 <dancanian25@gmail.com>
2025-04-15 12:12:23 +0300

    Rename uploadTime to uploadDate in Photo entity for clarity

--- src/main/java/com/mongs/restaurant/domain/entities/Photo.java
+++ src/main/java/com/mongs/restaurant/domain/entities/Photo.java
@@ -18,6 +18,6 @@ public class Photo {
     private String url;
 
     @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
-    private LocalDateTime uploadTime;
+    private LocalDateTime uploadDate;
 
 }
